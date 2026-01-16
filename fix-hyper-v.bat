8ce96fdfa94ef774acde7d5fc01243ff11083f94
alleydog <51403105+alleydog@users.noreply.github.com>
2026-01-16 10:39:37 +0300

    fix-hyper-v.bat

--- fix-hyper-v.bat
+++ fix-hyper-v.bat
@@ -4,3 +4,4 @@ for /f %%i in ('findstr /i . hyper-v.txt 2^>nul') do dism /online /norestart /ad
 del hyper-v.txt
 Dism /online /enable-feature /featurename:Microsoft-Hyper-V -All /LimitAccess /ALL
 pause
+
